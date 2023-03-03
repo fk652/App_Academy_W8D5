bde82b723abdd816e472523572f294f2c90ef782
Fahim Khan <fk652@nyu.edu>
2023-03-03 13:49:22 -0500

    finish sum

---
+++ arguments.js
@@ -0,0 +1,25 @@
+function sum() {
+  let total = 0;
+
+  for (let i = 0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+
+  return total;
+}
+
+console.log(sum(1, 2, 3, 4) === 10);
+console.log(sum(1, 2, 3, 4, 5) === 15);
+
+function sum2(...args) {
+  let total = 0;
+
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+
+  return total;
+}
+
+console.log(sum2(1, 2, 3, 4) === 10);
+console.log(sum2(1, 2, 3, 4, 5) === 15);
