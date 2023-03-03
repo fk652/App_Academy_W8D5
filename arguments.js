28e8b9fe32c0fbe5108c4cc0e1ebb1f1c9a00e16
Fahim Khan <fk652@nyu.edu>
2023-03-03 14:43:43 -0500

    finish curry

--- arguments.js
+++ arguments.js
@@ -1,36 +1,34 @@
-// function sum() {
-//   let total = 0;
+console.log("testing sum ➕");
+function sum() {
+  let total = 0;
 
-//   for (let i = 0; i < arguments.length; i++) {
-//     total += arguments[i];
-//   }
+  for (let i = 0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
 
-//   return total;
-// }
+  return total;
+}
 
-// console.log(sum(1, 2, 3, 4) === 10);
-// console.log(sum(1, 2, 3, 4, 5) === 15);
+console.log("sum1")
+console.log(sum(1, 2, 3, 4) === 10);
+console.log(sum(1, 2, 3, 4, 5) === 15);
+console.log();
 
-// function sum2(...args) {
-//   let total = 0;
+function sum2(...args) {
+  let total = 0;
 
-//   for (let i = 0; i < args.length; i++) {
-//     total += args[i];
-//   }
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
 
-//   return total;
-// }
+  return total;
+}
 
-// console.log(sum2(1, 2, 3, 4) === 10);
-// console.log(sum2(1, 2, 3, 4, 5) === 15);
+console.log("sum2")
+console.log(sum2(1, 2, 3, 4) === 10);
+console.log(sum2(1, 2, 3, 4, 5) === 15);
 
-// Function.prototype.myBind = function(context, ...bindArgs) {
-//   let that = this;
-
-//   return function(...callArgs) {
-//     return that.apply(context, bindArgs.concat(callArgs));
-//   }
-// };
+console.log('\ntesting bind 🪢');
 
 Function.prototype.myBind = function(context) {
   let that = this;
@@ -41,6 +39,14 @@ Function.prototype.myBind = function(context) {
     return that.apply(context, bindArgs.concat(callArgs));
   }
 
+};
+
+Function.prototype.myBind2 = function(context, ...bindArgs) {
+  let that = this;
+
+  return function(...callArgs) {
+    return that.apply(context, bindArgs.concat(callArgs));
+  }
 };
 
 class Cat {
@@ -63,7 +69,8 @@ class Dog {
 const markov = new Cat("Markov");
 const pavlov = new Dog("Pavlov");
 
-// markov.says("meow", "Ned");
+console.log('myBind1');
+markov.says("meow", "Ned");
 // Markov says meow to Ned!
 // true
 
@@ -83,7 +90,33 @@ markov.says.myBind(pavlov, "meow")("Markov");
 // true
 
 // no bind time args (other than context), call time args are "meow" and "me"
-const notMarkovSays = markov.says.myBind(pavlov);
+let notMarkovSays = markov.says.myBind(pavlov);
 notMarkovSays("meow", "me");
 // Pavlov says meow to me!
 // true
+
+console.log('\nmyBind2');
+markov.says("meow", "Ned");
+markov.says.myBind2(pavlov, "meow", "Kush")();
+markov.says.myBind2(pavlov)("meow", "a tree");
+markov.says.myBind2(pavlov, "meow")("Markov");
+notMarkovSays = markov.says.myBind2(pavlov);
+notMarkovSays("meow", "me");
+
+
+console.log('\ntesting curry 🍛')
+function curriedSum(numArgs) {
+  let numbers = [];
+
+  return function _curriedSum(num) {
+    numbers.push(num);
+    if (numbers.length === numArgs) {
+      return sum(...numbers);
+    } else {
+      return _curriedSum;
+    }
+  }
+}
+
+const total = curriedSum(4);
+console.log(total(5)(30)(20)(1)); // => 56
